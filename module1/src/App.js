204a44fc6177236f62d51a966d5cfe1a3f3754d5
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-03-25 13:15:35 -0400

    base

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
-import { Planet } from './Planet';
 
 
 
 function App() {
-  const planets=  [
-    {name: "Mars", isGasPlanet: false},
-    {name: "Earth", isGasPlanet: false},
-    {name: "Jupiter", isGasPlanet: true},
-    {name: "Venus", isGasPlanet: false},
-    {name: "Neptune", isGasPlanet: true},
-    {name: "Uranus", isGasPlanet: true}
-  ];
+
 
   //loop through planets and only display if gasplanet
   return <div className="App">
-   { planets.map(
-    (planet,key) => !planet.isGasPlanet && <Planet name={planet.name}/>
-  )}
+
+  
 
   </div>
 
--- module1/src/Planet.js
+++
@@ -1,3 +0,0 @@
-export const Planet = (props) => {
-    return <h1>{props.name}</h1>
-}
