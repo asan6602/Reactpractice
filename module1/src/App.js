e2045660d9821dcbb195f287cedaffcae40956ff
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-03-25 13:41:11 -0400

    state list

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+import {useState} from "react";
+import { entry } from './entry';
 
 
 
 function App() {
+  const [toDoList, setList] = useState([]);
+  const[newTask, setNewTask] = useState("");
 
+  const addtoList = () => {
+    //... array composesed of everything in first plus second entry
+    const newToDoList = [...toDoList, newTask]
+    setList(newToDoList)
+  };
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  };
 
   //loop through planets and only display if gasplanet
-  return <div className="App">
-
-  
+  return (
+  <div className="App">
+    <div className="addTask">
+      <input onChange={handleChange}></input>
+      <button onClick={addtoList}>Add Task</button>
+    </div>
+    <div className="list">
+      {toDoList.map((task) =>{
+        return entry(task)
+      })}
+    </div>
 
   </div>
-
+  );
 }
 
 
---
+++ module1/src/entry.js
@@ -0,0 +1,3 @@
+export const entry = (task) => {
+    return <h2>{task}</h2>
+}
